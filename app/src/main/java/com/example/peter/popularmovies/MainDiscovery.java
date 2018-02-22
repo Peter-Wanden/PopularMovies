0abd7cbad794c60d1840ad4678b19f0883687ecd
Peter.Wanden <peter.wanden@gmail.com>
2018-02-22 15:10:43 +0000

    initial commit

---
+++ app/src/main/java/com/example/peter/popularmovies/MainDiscovery.java
@@ -0,0 +1,13 @@
+package com.example.peter.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainDiscovery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_discovery);
+    }
+}
