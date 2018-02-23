1052d12d9c93bf9e5a3ebd2c81cb92f8737889ed
Peter.Wanden <peter.wanden@gmail.com>
2018-02-23 12:17:40 +0000

    add JSON Utility class

--- app/src/main/java/com/example/peter/popularmovies/MainDiscovery.java
+++ app/src/main/java/com/example/peter/popularmovies/MainDiscovery.java
@@ -1,9 +1,15 @@
 package com.example.peter.popularmovies;
 
+import android.app.LoaderManager;
+import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainDiscovery extends AppCompatActivity {
+import com.example.peter.popularmovies.model.Movie;
+
+import java.util.List;
+
+public class MainDiscovery extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Movie>> {
 
     // API Key TODO - Make this key a user input under preferences
     private static final String API_KEY = "api_key=dc92a2a6c5b67810baa769bdd918b776";
@@ -28,5 +34,20 @@ public class MainDiscovery extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_discovery);
+    }
+
+    @Override
+    public Loader<List<Movie>> onCreateLoader(int i, Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Movie>> loader, List<Movie> movies) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Movie>> loader) {
+
     }
 }
